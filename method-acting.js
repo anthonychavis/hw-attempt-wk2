973d854e2a00bf2f2727a2bbb5b9cf1325a4ca0e
Anthony C <gitanthony@yahoo.com>
2023-05-27 17:15:52 -0400

    using a Class with getters

--- method-acting.js
+++ method-acting.js
@@ -1,5 +1,48 @@
 'use strict';
 
+class Person {
+    constructor(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    get firstName() {
+        return this._firstName.toUpperCase();
+    }
+
+    get lastName() {
+        return this._lastName.toLowerCase();
+    }
+
+    get wholeName() {
+        return this.firstName.concat(' ', this.lastName);
+    }
+
+    get wholeNameNormal() {
+        return this._firstName.concat(' ', this._lastName);
+    }
+
+    get sumLettersOfWholeName() {
+        return this.wholeName
+            .toLowerCase()
+            .split('')
+            .filter(
+                letter =>
+                    letter.charCodeAt(0) >= 'a'.charCodeAt(0) &&
+                    letter.charCodeAt(0) <= 'z'.charCodeAt(0)
+            ).length;
+    }
+}
+
+const anthony = new Person('Anthony', 'Chavis');
+
+console.log(anthony.lastName);
+console.log(anthony.firstName);
+console.log(anthony.wholeName, anthony.wholeNameNormal);
+console.log(anthony.sumLettersOfWholeName);
+
+/*
+// before using a Class
 let firstName = 'Anthony',
     lastName = 'Chavis',
     wholeName,
@@ -24,3 +67,4 @@ wholeName
     });
 
 console.log(sumLettersOfWholeName);
+*/
